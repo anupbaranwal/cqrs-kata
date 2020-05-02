3cfe24dabefc42b5e284ff2d835a8e4b924a1032
Anup Kumar <anup.raj1989@gmail.com>
2020-05-02 15:05:28 +0530

    feat: add query controller and operations

---
+++ src/main/java/com/kata/cqrs/model/Student.java
@@ -0,0 +1,20 @@
+package com.kata.cqrs.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+  private Long registrationNumber;
+  private String name;
+  private String email;
+  private String yearOfRegistration;
+  private String section;
+  private Address address;
+}
